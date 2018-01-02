db41159f62a7b7a85709419604bf88964a3b0dc4
HY_RORRE <hy_rorre@hotmail.com>
2018-01-02 09:36:05 +0900

    Set music title to <title> on show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+# app/helpers/application_helper.rb
 module ApplicationHelper
-end
+	def full_title(page_title)
+	  base_title = "ミリシタ簡易譜面ビューア" # 自分のアプリ名を設定する
+	  if page_title.empty?
+		base_title
+	  else
+		"#{page_title} | #{base_title}"
+	  end
+	end
+  end
