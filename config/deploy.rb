8391d898a5b75fad4ae60db746d953b7b2a63913
HY_RORRE <hy_rorre@hotmail.com>
2018-01-08 19:28:14 +0900

    Setting for deployment (WIP37)

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :repo_url, "git@bitbucket.org:HY_RORRE/millionguide.git"
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
-set :linked_files, fetch(:linked_files, []).push('config/settings.yml', 'config/puma.rb', ".rbenv_vars")
+set :linked_files, fetch(:linked_files, []).push('config/settings.yml', 'config/puma.rb')
 
 # シンボリックリンクをはるフォルダ。(※後述)
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
