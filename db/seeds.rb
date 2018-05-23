37e63af6d4a8e0162cdbb7ea000c54e00bc9d00c
hyrorre <hy_rorre@hotmail.com>
2018-05-24 00:54:09 +0900

    Add songs

--- db/seeds.rb
+++ db/seeds.rb
@@ -412,6 +412,21 @@ Music.create(
     level_mmp: 0
 )
 Music.create(
+    idstr: "hweekend",
+    title: "花ざかりWeekend✿",
+    kana: "はなざかりWeekend",
+    musictype: 2,
+    order: 2022,
+    added_on: Date.new(2018, 5, 21),
+    updated_on: Date.new(2018, 5, 21),
+    level_2m: 4,
+    level_2mp: 11,
+    level_4m: 7,
+    level_6m: 12,
+    level_mm: 17,
+    level_mmp: 0
+)
+Music.create(
     idstr: "sutekise",
     title: "素敵なキセキ",
     kana: "すてきなきせき",
@@ -662,6 +677,21 @@ Music.create(
     level_2m: 3,
     level_2mp: 10,
     level_4m: 6,
+    level_6m: 11,
+    level_mm: 16,
+    level_mmp: 0
+)
+Music.create(
+    idstr: "happyeffect",
+    title: "ハッピ～ エフェクト！",
+    kana: "はっぴーえふぇくと",
+    musictype: 2,
+    order: 3018,
+    added_on: Date.new(2018, 5, 13),
+    updated_on: Date.new(2018, 5, 13),
+    level_2m: 4,
+    level_2mp: 10,
+    level_4m: 7,
     level_6m: 11,
     level_mm: 16,
     level_mmp: 0
