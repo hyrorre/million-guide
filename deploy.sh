9c434bddda2f9144b59bd092d989e5b4905d71ed
hyrorre <hy_rorre@hotmail.com>
2018-09-09 23:19:37 +0900

    Bypass html folder existence check

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,14 @@
 cd `dirname $0`
 DIR=$(cd $(dirname $0); pwd)
 
-if [ -e html ]; then
-	echo "Directory 'html' is already exist."
-else
-	rails db:seed
-	rails sitemap:create
-	rails s -d
-	ruby output.rb
-	kill -9 `cat tmp/pids/server.pid`
-	ln -s $DIR/app/assets/images/jackets $DIR/app/assets/images/musictypes html/assets/
-	rsync -acvL html/ lolipop:~/web/million/
-	rm -r html
-	rails sitemap:refresh
-fi
+rails db:seed
+rails sitemap:create
+rails s -d
+ruby output.rb
+kill -9 `cat tmp/pids/server.pid`
+ln -s $DIR/app/assets/images/jackets $DIR/app/assets/images/musictypes html/assets/
+rsync -acvL html/ lolipop:~/web/million/
+rm -r html
+rails sitemap:refresh
 
 exit 0
