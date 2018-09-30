260a71481dbd359a873710b02e24c664ad6173b6
hyrorre <hy_rorre@hotmail.com>
2018-10-01 03:32:45 +0900

    Optimize deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 cd `dirname $0`
-DIR=$(cd $(dirname $0); pwd)
 
 rails db:seed
 rails sitemap:create
 rails s -d
 ruby output.rb
 kill -9 `cat tmp/pids/server.pid`
-rsync -acvL html/ lolipop:~/web/million/
+rsync -a --delete html/ lolipop:~/web/million/
 rm -r html
 rails sitemap:refresh
 
